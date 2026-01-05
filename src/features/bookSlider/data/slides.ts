6e895c5ee7c46313744479fe2011544ce0fdca14
DonnaTelloo <threeunexpectedperson@gmail.com>
2026-01-05 18:23:47 +0400

    changes

--- src/features/bookSlider/data/slides.ts
+++ src/features/bookSlider/data/slides.ts
@@ -18,7 +18,7 @@ export const slides: BookSlide[] = [
         },
         title: 'Italian Aircraft in the Georgian Armed Forces',
         text: `According to the 1920 newspaper “Republic of Georgia,“ the Democratic Republic of Georgia commissioned pilot Mikheil Machavariani to procure Italian aircraft. Colonel Mikheil Machavariani, head of the aviation team, departed for Italy in March 1920. He traveled alongside Senator Ettore Conti’s civilian delegation, which was returning from their official visit to Tbilisi.
-Machavariani’s mission proved successful, earning commendation from the Minister of Defense. His negotiations resulted in the Georgian government’s acquisition of ten SVSA-10 aircraft from Italy’s Ansaldo factory. The Ansaldo aircraft obtained by the Georgian army featured robust 250-horsepower engines. However, as they were purchase without armament, these aircraft were restricted to reconnaissance operations.`,
+Machavariani’s mission proved successful, earning commendation from the Minister of Defense. His negotiations resulted in the Georgian government’s acquisition of ten SVSA-10 aircraft from Italy’s Ansaldo factory. The Ansaldo aircraft obtained by the Georgian army featured robust 250-horsepower engines`,
         image: '/src/assets/plane.png',
     },
 ];
