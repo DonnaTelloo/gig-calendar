614ca98a1ea16da7b6db19f572ee278b10b3cf01
donnatelloo <threeunexpectedperson@gmail.com>
2026-01-28 15:17:04 +0400

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import {BrowserRouter} from "react-router-dom";
 import AppRoutes from "./routes";
 import "./styles/global.css";
+import {HelmetProvider} from "react-helmet-async";
 
 export default function App() {
     return (
         <BrowserRouter>
-            <AppRoutes />
+            <HelmetProvider>
+                <AppRoutes />
+            </HelmetProvider>
         </BrowserRouter>
     );
 }
